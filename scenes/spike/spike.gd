542de5f00033ac718dc7c7142f83a920feb0e907
SirLich <lrkoehler77@gmail.com>
2026-01-31 21:58:00 +0100

    Import spike sfx

--- scenes/spike/spike.gd
+++ scenes/spike/spike.gd
@@ -1,7 +1,14 @@
 extends Node2D
 @export var animation_player: AnimationPlayer
+@export var spike_sound : AudioStream
 
 func _ready() -> void:
 	animation_player.play("spawn")
+	await Utils.wait(1.0)
+	if randf() > 0.7:
+		SoundManager.play_sound(spike_sound)
+
 	await animation_player.animation_finished
+	
+	
 	self.queue_free()
