b6e10c07fe5f533bdda36486189f3f18a86616b0
SirLich <lrkoehler77@gmail.com>
2026-01-31 19:05:23 +0100

    Add assets

--- scenes/anger/anger.gd
+++ scenes/anger/anger.gd
@@ -8,6 +8,13 @@ class_name Anger
 @export var dash_distance = 700
 @export var fire_pause = 0.1
 @export var dash_speed = 0.4
+@export var hurt_animation: AnimationPlayer
+@export var death_animation: AnimationPlayer
+@export var next_enemy : PackedScene
+
+
+@export var transition_audio : AudioStream
+var is_dead = false
 
 func _ready() -> void:
 	do_intro()
@@ -22,12 +29,30 @@ func do_intro():
 	SoundManager.play_music(angry_music)
 	
 func on_hurt():
-	pass
+	hurt_animation.play("hurt")
 
 func on_died():
-	pass
+	Global.enemy_died.emit()
+	is_dead = true
+	play_audio(transition_audio)
+	await Utils.wait(7.9)
+	death_animation.play("die")
+	await death_animation.animation_finished
+	var new_enemy = next_enemy.instantiate()
+	new_enemy.global_position = global_position
+	add_sibling(new_enemy)
+	self.queue_free()
+
+func play_audio(audio, is_voice = false):
+	if Global.settings.skip_audio:
+		return
+
+	var new_audio = SoundManager.play_sound_with_pitch(audio, Global.settings.audio_speed)
 	
 func do_dash():
+	if is_dead:
+		return
+		
 	var dir = Utils.get_direction_to_player(self)
 	var pos = global_position + (dir * dash_distance)
 	
