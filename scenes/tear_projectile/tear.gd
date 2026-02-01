d6f28c1450a09f07bcfa6dd695de31143ad98d24
SirLich <lrkoehler77@gmail.com>
2026-02-01 12:58:47 +0100

    Make raindrops killable

--- scenes/tear_projectile/tear.gd
+++ scenes/tear_projectile/tear.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 @export var speed = 400
 @export var animation_player: AnimationPlayer
+@export var health_component: HealthComponent
 
 func _physics_process(delta: float) -> void:
 	global_position += Vector2.DOWN.rotated(rotation) * delta * speed
-
+	
+func died():
+	queue_free()
+	
 func _ready() -> void:
+	health_component.died.connect(died)
 	await animation_player.animation_finished
 	queue_free()
